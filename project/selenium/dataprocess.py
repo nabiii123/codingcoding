f474b959558cd0c13c8886241836cc952e813df0
hellohasalbub <88815671+nabiii123@users.noreply.github.com>
2021-11-11 10:14:20 +0900

    작은 수정
    사진수정, 코딩 조금 함

--- project/selenium/dataprocess.py
+++ project/selenium/dataprocess.py
@@ -9,7 +9,31 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 
+import os
+
+#__________________________________
+
 URL = 'https://teachablemachine.withgoogle.com/train/image'
 
-driver = webdriver.Chrome(executable_path='chromedriver')
-driver.get(url=URL)
+# driver = webdriver.Chrome(executable_path='chromedriver')
+# driver.get(url=URL)
+
+# add_class=driver.find_element_by_id("add-classes")
+# for a in range(3):
+#     add_class.click()
+
+# menu={
+#     "day1":["egg","kimchi","namul","rice","soup"],
+#     "day2":["hobak","kimchi","potato","rice","soup"]
+#     }
+
+def fileroot():
+    dir_path = "Users\이채은\OneDrive\바탕 화면\codingcoding\project\studydata_day1\rice"
+    # +day+"/"+menu["day"+day][n]
+    for (root, directories, files) in os.walk(dir_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            print(file_path)
+
+
+fileroot()
