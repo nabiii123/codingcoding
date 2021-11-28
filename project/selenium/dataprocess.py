2f52026bd13bcd46e00fbd8a3a063a119d5785bf
hellohasalbub <88815671+nabiii123@users.noreply.github.com>
2021-11-28 23:05:14 +0900

    절거운 코딩^^

--- project/selenium/dataprocess.py
+++ project/selenium/dataprocess.py
@@ -32,59 +32,82 @@ menu_result={
     "day2":[[],[],[],[],[]]
     }
 
+def shadow_element(elem, query):
+    return driver.execute_script(f"return arguments[0].shadowRoot.querySelector('{query}')", elem)
+
 for key in menu:
     menukey=0
     for food in menu[key]:
         driver.get(url=URL)
-        add_class=driver.find_element_by_class_name('add-classes')#class 추가
+
+        root1 = driver.find_element_by_id('tmApp')
+        root2 = shadow_element(root1, '#classifier-list')
+        add_class = shadow_element(root2, '.add-classes')
         for a in range(3):
-            add_class.click()
+            add_class.send_keys('\n')
+
         for percent in range(0,101,25):
-            file_path=os.listdir('C:₩₩Users₩₩이채은₩₩OneDrive₩₩바탕 화면₩₩포트폴리오₩₩codingcoding₩₩project₩₩studydata_'+key+"₩"+food+str(percent)) #폴더안의 파일 주소 가져오기
-            upload_=driver.find_element_by_css_selector("#sample-input-list > button:nth-child(2)")
+            file_path=os.listdir('C:/Users/이채은/OneDrive/바탕 화면/포트폴리오/codingcoding/project/studydata_'+key+"/"+food+"/"+str(percent)) #폴더안의 파일 주소 가져오기
+            r='#classifier-list > tm-classifier-drawer:nth-child('+str(menukey+1)+')'
+            print(r)
+            root3 = shadow_element(root2, 'tm-classifier-drawer:nth-child('+str(menukey+1)+')')
+            upload_=shadow_element(root3, '#sample-input-list > button:nth-child(2)')
             upload_.click()
-            studydata_input=driver.find_element_by_id("file-input")
+
+            exit_button=shadow_element(root3,'#exit-button')
+            root4=shadow_element(root3,'tm-file-sample-input')
+            studydata_input=shadow_element(root4,'#file-input')
             for study in file_path:
                 studydata_input.send_keys(study)
-        start_study=driver.find_element_by_css_selector("#container > tm-button")
-        select = driver.find_element_by_id('select-input')
+                exit_button.send_keys('\n')
+                
+        study_root2=shadow_element(root1,'#train')
+        study_root3=shadow_element(study_root2,'#train-btn')
+        start_study=shadow_element(study_root3,'#container > tm-button')
+        start_study.send_keys('\n')
+        
+        run_root2=shadow_element(root1,'#run')
+        run_root3=shadow_element(run_root2,'#body-container > div.section.no-border > div > div:nth-child(2) > tm-bar-graph')
+        select=shadow_element(run_root2,'#select-input')
         select.select_by_visible_text("파일")
-        sikpan_upload=driver.find_element_by_id('file-input') #결과도출&리스트 저장
-        sikpan_pic_path=os.listdir(".₩₩Users₩₩이채은₩₩OneDrive₩₩바탕 화면₩₩포트폴리오codingcoding₩₩project₩₩studydata_₩₩sikpan_"+key+"₩₩"+food)
+        sikpan_upload=shadow_element(run_root2,'#file-input') #결과도출&리스트 저장
+        sikpan_pic_path=os.listdir("./Users/이채은/OneDrive/바탕 화면/포트폴리오codingcoding/project/sikpan_"+key+"/"+food)
         for file in sikpan_pic_path:
             sikpan_upload.send_key(file)
-            individual_result=driver.find_elements_by_id("value-label")
-            menu_result[key][menukey].append(individual_result)
+            for q in range(5):
+                run_root3=shadow_element(run_root2,'#body-container > div.section.no-border > div > div:nth-child('+str(q+2)+') ' '> tm-bar-graph')
+                individual_result=shadow_element(run_root3,'#value-label')
+                menu_result[key][menukey].append(individual_result)
         menukey=menukey+1
         driver.close()
 
-#%제거
-for key in menu_result:
-    for n in key:
-        for m in n:
-            for number in m:
-                m=float((m.replace("%","")))
+# #%제거
+# for key in menu_result:
+#     for n in key:
+#         for m in n:
+#             for number in m:
+#                 m=float((m.replace("%","")))
 
 print(menu_result)
 
-######여기부터 colab으로 이동#######
+# ######여기부터 colab으로 이동#######
 
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plot
+# import numpy as np
+# import pandas as pd
+# import matplotlib.pyplot as plot
 
-#단일 값 만들기
-for key in menu_result:
-    for n in key:
-        np_array=np.array(n)
-        sum_array=np_array.sum(axis=1)
-        target_array=sum_array/100
+# #단일 값 만들기
+# for key in menu_result:
+#     for n in key:
+#         np_array=np.array(n)
+#         sum_array=np_array.sum(axis=1)
+#         target_array=sum_array/100
 
-#정규분포 그리기&저장
-for key in menu_result:
-    menukey=0
-    for n in key:
-        plt.title(menu[key][menukey]+"의 잔반")
-        plt.hist(n)
-        menukey=menukey+1
-        plt.savefig('.₩₩'+menu[key][menukey]+'sin.png')
+# #정규분포 그리기&저장
+# for key in menu_result:
+#     menukey=0
+#     for n in key:
+#         plt.title(menu[key][menukey]+"의 잔반")
+#         plt.hist(n)
+#         menukey=menukey+1
+#         plt.savefig('./'+menu[key][menukey]+'sin.png')
